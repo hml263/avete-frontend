12db3d5ef9d7dd093c0ef0e108993742b6691ac0
hml263 <htetmyetlwin263@gmail.com>
2024-07-22 05:21:15 +0700

    updated hero section

--- components/heroGlobe.tsx
+++ components/heroGlobe.tsx
@@ -7,21 +7,21 @@ import { motion } from 'framer-motion'
 const HeroGlobe = () => {
   return (
     <div className='relative items-center justify-center'>
-       {/* <Image
-              src='/globe.gif' height={600} width={2000} alt='globe'
-               className="inset-0 z-80 object-contain
-               xl:translate-y-[1px]" />   */}
+      
 
 <video
         autoPlay
         loop
         muted
-        className=" z-10 w-[700px] min-w-full min-h-full max-w-none"
+        className=" z-10 w-[700px] min-w-full min-h-full max-w-none xl:mt-[-80px]"
       >
         <source src="/globe.mp4" type="video/mp4" />
       </video>
 
-          
+           <Image
+              src='/logo.png' height={80} width={280} alt='globe'
+               className="inset-0 z-80 object-contain xl:mt-[-220px] xl:ml-[210px]
+               xl:translate-y-[1px]" style={{ left: '30%',}} />  
         </div>
    
   )
