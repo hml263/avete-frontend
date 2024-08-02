ea7f70786113f1a8342b66759b6b67e16de1c774
hml263 <htetmyetlwin263@gmail.com>
2024-08-02 18:37:18 +0700

    updated product page

--- components/product.tsx
+++ components/product.tsx
@@ -9,7 +9,7 @@ const Product = () => {
   return (
     <section id='product' className="z-30 translate-y-1 pt-[150px]">
       <div className="container w-full ">
-        <div className="flex flex-col justify-between gap-12 px-2 lg:flex-row lg:px-0">
+        <div className="flex flex-col justify-between gap-12 px-2 lg:flex-row lg:px-0 mb-[150px]">
           <div
             className="justify-between lg:mr-8 ">
             <h1 className='text-[30px] font-medium  leading-[3rem] pb-[30px]'>
@@ -18,64 +18,65 @@ const Product = () => {
 
             <p className="text-justify ">{t('p-description')}
 
-            </p><br />
-            <br /><br />
-            <div className="hidden xl:grid grid-cols-4 gap-4 ">
-              <div className="">
-                <div className=" vertical-text mt-[180px] mb-[70px]">{t('p-glass')}
-
-                  <Image src='/img-01.png' height={200} width={200} alt='mission' className="relative right-[-160px] vertical-img rounded-xl" />
+            </p>
 
 
-                </div>
-              </div>
-              <div>
+          </div>
 
-                <Image
-                  src='/img-04.png' height={100} width={130} alt='mission' className="relative top-[50px] rounded-xl" />
-
-              </div>
-
-
-              <div></div>
-              <div className="text-center text-xl relative left-[-130px] top-[60px]">{t('protocols')}<br />
+        </div>
+        <table className="hidden xl:flex table-auto mb-[150px]  items-center justify-center ">
+          <tbody>
+            <tr>
+              <td> <div className=" vertical-text relative bottom-4">{t('p-glass')}</div></td>
+              <td> <Image src='/img-01.png' height={150} width={150} alt='mission' className="relative right-[80px] bottom-[80px] rounded-xl" /></td>
+              <td><div className="w-[80px] h-[1px] bg-white relative right-[80px] bottom-[75px]"></div></td>
+              <td><Image src='/img-04.png' height={90} width={100} alt='mission' className="relative right-[80px] bottom-[80px] rounded-xl" /></td>
+              <td>
+                {/* <div className="w-[80px] h-[1px] bg-white relative right-[80px] bottom-[75px]"></div> */}
+              </td>
+              <td></td>
+              <td></td>
+              <td><div className="w-[300px] text-center font-bold text-[24px] text-xl relative right-[100px]  ">{t('protocols')}<br />
                 ⇩
-              </div>
-              <div className=""><div className="vertical-text ">{t('p-phone')}
-                <Image
-                  src='/img-02.png' height={200} width={200} alt='mission' className="relative right-[-160px] vertical-img rounded-xl" /></div></div>
-              <div><Image
-                src='/img-05.png' height={100} width={130} alt='mission' className="relative top-[-140px] rounded-xl" /></div>
-              <div><Image
-                src='/img-07.png' height={100} width={180} alt='mission' className="relative top-[-140px] right-[80px] rounded-xl" /></div>
-              <div>
+              </div></td>
+
+            </tr>
+            <tr>
+              <td><div className=" vertical-text relative top-16 ">{t('p-glass')}</div></td>
+              <td><Image src='/img-02.png' height={150} width={150} alt='mission' className="relative right-[80px] bottom-[0px] rounded-xl" /></td>
+              <td><div className="w-[80px] h-[1px] bg-white relative right-[80px] bottom-[0px]"></div></td>
+              <td><Image src='/img-05.png' height={90} width={100} alt='mission' className="relative right-[80px] bottom-[0px] rounded-xl" /></td>
+              <td><div className="w-[80px] h-[1px] bg-white relative right-[80px] bottom-[0px]"></div></td>
+              <td><Image src='/ai.png' height={180} width={200} alt='mission' className="relative right-[90px] bottom-[0px] rounded-xl" /></td>
+              <td><div className="w-[80px] h-[1px] bg-white relative right-[100px] bottom-[0px]"></div></td>
+              <td><div className="relative right-[20px] top-[180px]">
                 <table className="border-separate border-spacing-0 border border-slate-500 ... relative top-[-180px] right-[80px]">
 
                   <tbody>
                     <tr>
-                      <td className="border border-slate-700 ...">How many stepladders are present?
+                      <td className="border border-slate-700 ...">{t('p-q1')}
                       </td>
                       <td className="border border-slate-700 ...">1</td>
                     </tr>
                     <tr>
-                      <td className="border border-slate-700 ...">Is stepladder properly secured?
+                      <td className="border border-slate-700 ...">{t('p-q2')}
                       </td>
                       <td className="border border-slate-700 ...">Yes</td>
                     </tr>
                     <tr>
-                      <td className="border border-slate-700 ...">Is handrail installed?
+                      <td className="border border-slate-700 ...">{t('p-q3')}
                       </td>
                       <td className="border border-slate-700 ...">Yes</td>
                     </tr>
                     <tr>
-                      <td className="border border-slate-700 ...">How many workers are working?
+                      <td className="border border-slate-700 ...">{t('p-q4')}
 
                       </td>
                       <td className="border border-slate-700 ...">0</td>
                     </tr>
 
                     <tr>
-                      <td className="border border-slate-700 ...">  Is worker working on Stepladder?
+                      <td className="border border-slate-700 ...">{t('p-q5')}
 
                       </td>
                       <td className="border border-slate-700 ...">No</td>
@@ -84,23 +85,30 @@ const Product = () => {
 
                   </tbody>
                 </table>
+              </div></td>
 
-              </div>
+            </tr>
+            <tr>
+              <td><div className=" vertical-text relative top-[160px]">{t('p-glass')}</div></td>
+              <td><Image src='/img-03.png' height={150} width={150} alt='mission' className="relative right-[80px] top-[90px] rounded-xl" /></td>
+              <td><div className="w-[80px] h-[1px] bg-white relative right-[80px] top-[90px]"></div></td>
+              <td><Image src='/img-06.png' height={90} width={100} alt='mission' className="relative right-[80px] top-[90px] rounded-xl" /></td>
+              <td>
+                {/* <div className="w-[80px] h-[1px] bg-white relative right-[80px] top-[90px]"></div> */}
+              </td>
+              <td><div className="w-[120px] text-center relative right-[50px] bottom-[20px]">{t('p-ai')}</div>
+              </td>
+              <td></td>
+              <td></td>
 
 
+            </tr>
 
-
-              <div className=""><div className="vertical-text ">{t('p-cam')}<Image
-                src='/img-03.png' height={200} width={200} alt='mission' className="relative right-[-110px] vertical-img rounded-xl" /></div></div>
-              <div><Image
-                src='/img-06.png' height={100} width={130} alt='mission' className="relative top-[-90px] rounded-xl" /></div>
-              <div></div>
-              <div></div>
-
-            </div>
-          </div>
-
-        </div>
+          </tbody>
+        </table>
+        <div className="hidden xl:flex relative left-[120px] bottom-[50px]">{t('p-capture')}       </div>
+        <div className="w-[200px] h-[1px] bg-white relative bottom-[500px] left-[340px] rotate-60  "></div>
+        <div className="w-[200px] h-[1px] bg-white relative bottom-[240px] left-[340px] rotate-60down  "></div>
       </div>
     </section>
   )
